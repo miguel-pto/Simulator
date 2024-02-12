ed55cafde6f86ee0742d5f460d7e2193d9a9f4ce
nreneses <nreneses@ucm.es>
2024-02-12 10:53:02 +0100

    Simulator_again

--- Pr1/src/src/simulator/model/Simulator.java
+++ Pr1/src/src/simulator/model/Simulator.java
@@ -1,50 +1,67 @@
 package src.simulator.model;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.json.JSONObject;
 
 public class Simulator {
-	RegionManager RegionManager;
+	RegionManager region_manager;
 	List<Animal> animals_list;
-	double dt;
+	double t;
 	public Simulator(int cols, int rows, int widht, int height, Factory<Animal> animals_factory, Factory<Region> regions_factory) {
-		//Crea un nuevo manager
-		//Crea lista de animales
-		dt = 0.0;
+		region_manager = new RegionManager(cols, rows, widht, height);
+		//Crear lista de animales 
+		//animals_list = 
+		t = 0.0;
 	}
 	
 	
 	
 	private void set_region(int row, int col, Region r) {
-		//Con el manager lo mete en las pos row, col
+		region_manager.set_region(row, col, r);
 	}
 	
 	void set_region(int row, int col, JSONObject r_json) {
+		//TODO Que lea el json y cree la region/animal
+		
 		//Region R = new Region();
 		set_region(row, col, R);
 	}
 	
 	private void add_animal(Animal a) {
-		//Necesito gestor de regiones
+		region_manager.register_animal(a);
 	}
 	
 	void add_animal(JSONObject a_json) {
 		String type = a_json.getString(type);
-		//TODO 
+		//TODO
+		
 		Animal A = new Animal(type, );
 		add_animal(A);
 	}
 	
-	MapInfo get_map_info() {
-		return RegionManager;
+	public MapInfo get_map_info() {
+		return region_manager;
 	}
 	
-	public List<? extends AnimalInfo> get_animals(){
-		//Devuelve una versión inmodificable de la lista de animales
+	public List<? extends AnimalInfo> get_animals() {
+		return Collections.unmodifiableList(animals_list);
 	}
 	
 	public double get_time() {
-		return dt;
+		return t;
+	}
+	
+	public void advance(double dt) {
+		t += dt;
+		Animal a;
+		for (int i= animals_list.size(); i>=0; i--) {
+			a = animals_list.get(i);
+			if (a.get_state() == State.DEAD) {
+				//TODO Hay que quitar de la lista el animal
+				region_manager.unregister_animal(a);
+			}
+		}
 	}
 }
