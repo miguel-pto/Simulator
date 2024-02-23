677ae0d5fdac731b17d381517714248721ddc717
nreneses <nreneses@ucm.es>
2024-02-23 12:16:18 +0100

    Controller

--- Pr1/src/simulator/control/Controller.java
+++ Pr1/src/simulator/control/Controller.java
@@ -1,12 +1,22 @@
 package simulator.control;
 
 import java.io.OutputStream;
+import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import simulator.model.Simulator;
 
+
 public class Controller {
+
+	public static final String REGIONES = "regions";
+	public static final String ROW = "row";
+	public static final String COL = "col";
+	public static final String SPEC = "spec";
+	public static final String ANIMALS = "animals";
+	public static final String AMOUNT = "amount";
 
 	private Simulator sim;
 
@@ -15,7 +25,31 @@ public class Controller {
 	}
 
 	public void load_data(JSONObject data) {
-		// TODO SABER Q COÑO ES UN JSON
+		JSONArray regiones = data.getJSONArray(REGIONES);
+		if (!regiones.isEmpty()) {
+			for (int i=0; i<regiones.length(); i++) {
+				JSONObject region = (JSONObject) regiones.get(i);
+				int row[] = (int[]) region.get(ROW);
+				int col[] = (int[]) region.get(COL); 
+				JSONObject spec = region.getJSONObject(SPEC); 
+				for (int j=row[0]; j<=row[1]; j++) {
+					for (int k = col[0]; k<=col[1]; k++) {
+						sim.set_region(k, k, spec);
+					}
+				}
+				
+			}
+		}
+		
+		JSONArray animales = data.getJSONArray(ANIMALS);
+		for (int i=0; i<animales.length(); i++) {
+			JSONObject animal = (JSONObject) animales.get(i);
+			int cant = animal.getInt(AMOUNT);
+			JSONObject spec = animal.getJSONObject(SPEC);
+			for (int j=0; j<cant; j++)
+				sim.add_animal(spec);
+		}
+		// TODO REVISAR Y QUE ESTE BIEN
 	}
 
 	public void run(double t, double dt, boolean sv, OutputStream out) {
@@ -24,4 +58,6 @@ public class Controller {
 		}
 		// TODO EL OUTPUT Y EL VISOR DE OBJETOS
 	}
+	
+	
 }
